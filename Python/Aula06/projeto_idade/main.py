75925201b62ddfd3442572aed65f469b3a951fdf
Verônica Rizzi <vevarizzi@gmail.com>
2024-11-25 18:51:38 -0300

    Atualizando aulas 5 e 6.

---
+++ Python/Aula06/projeto_idade/main.py
@@ -0,0 +1,10 @@
+from Aula06.projeto_idade.conversor_idade import conversor_idade
+
+dias = int(input("Digite a idade em dias: "))
+
+anos, meses, dias = conversor_idade(dias)
+
+
+print(f"{anos} ano(s)")
+print(f"{meses} mes(es)")
+print(f"{dias} dia(s)")
